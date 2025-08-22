74f4802eaffb06acb7f2563e2cc6b377026600a4
riro9510 <riro9510@gmail.com>
2025-08-22 01:38:28 -0600

    logs

--- backend/src/services/donations.service.ts
+++ backend/src/services/donations.service.ts
@@ -5,16 +5,30 @@ import { encryptAES } from '../utils/crypto.js';
 type IDonationCreate = Omit<IDonation, 'id' | 'createdAt' | 'updatedAt'>;
 
 const create = async (data: IDonationCreate) => {
-  const register = await Donations.create({
+  try{
+    const register = await Donations.create({
     ...data,
     donorEmail: encryptAES(data.donorEmail),
     donorName: encryptAES(data.donorName),
   });
   return register;
+  }
+  catch (error:any) {
+    console.error('Error creating donation:', error);
+    throw new Error('Failed to create donation: ' + error.toString());
+  }
 };
 
 const getAll = async () => {
-  return await Donations.findAll();
+  try{
+     const donations = await Donations.findAll();
+    console.log('Donations fetched:', donations.length);
+    return donations;
+  }
+  catch (error:any) {
+    console.error('Error fetching donations:', error);
+    throw new Error('Failed to fetch donations: ' + error.toString());
+  }
 };
 
 export default {
